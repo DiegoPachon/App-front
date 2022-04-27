5e164c5ac3b600637b2b07f17ecadaa39a4e2cca
Diego Pachon <diegopachon1@hotmail.com>
2022-04-27 17:15:21 -0500

    Sexto commit

--- frontend/src/Login/Login.jsx
+++ frontend/src/Login/Login.jsx
@@ -1,27 +1,65 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 import './Login.css';
 
 
 function Login() {
+
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);// caracteristicas que solo puenden ser alteradas por este componente
+  //muestra los mensajes de error, respuesta del back
+  const renderErrorMessage = (name) =>
+  name === errorMessages.name && (
+    <div className="error">{errorMessages.message}</div>
+  );
+  // método de activacion al oprimir iniciar sesión
+  const handleSubmit = (event) => {
+    // Recarga de pagina
+    console.log("Boton");
+    let { email, password } = document.forms[0];
+    console.log(password.value);
+    console.log(email.value);
+    const headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      }
+     axios({
+      method: 'post',
+      url:"http://localhost:3001/login",
+      data:{
+        email: email.value,
+        password: password.value
+      },
+      headers:headers
+     }
+    ).then((response)=> {
+      console.log(response.data);
+      event.preventDefault();
+    })
+
+   // event.preventDefault();
+  };
+
+
   return (
 
     <div className="Login">
         <title className='Title'>Bienvenido</title>
         <h1 className='Description'>Lo mejor de la sasón, está en el sabor</h1>
         <div className="Cajas">
-        <form className="Form">
+        <form className="Form" onSubmit={handleSubmit}>
             <div className="col-auto">
-            <label for="user" class="visually-hidden">User</label>
-            <input type="text" class="form-control form-control-lgl" id="" placeholder="Usuario"/>
+            <label htmlFor="user" className="visually-hidden">User</label>
+            <input type="text" className="form-control form-control-lgl" name="email" placeholder="Usuario"/>
             </div>
             <hr />
             <div className="col-auto">
-            <label for="password" class="visually-hidden">Password</label>
-            <input type="password" class="form-control" id="" placeholder="Contraseña"/>
+            <label htmlFor="password" className="visually-hidden">Password</label>
+            <input type="password" className="form-control" name="password" placeholder="Contraseña"/>
             </div>
             <hr />
             <div className="col-auto">
-            <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
+            <button type="submit" className="btn btn-primary">Iniciar Sesión</button>
             </div>  
         </form>
         </div>
